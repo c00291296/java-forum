c6c8aba7918e8abbcaca96e766d9b400abd7c53f
(Student  C00291296) Ihor Antonov <C00291296@setu.ie>
2024-01-18 09:54:40 +0000

    Successfully test database connection

--- Program.java
+++ Program.java
@@ -5,6 +5,18 @@ import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
-        connection
+        try {
+            Class.forName("org.sqlite.JDBC");
+            var c = DriverManager.getConnection("JDBC:sqlite:forum.db");
+            var s = c.createStatement();
+            System.out.println("Current users:");
+            var rs = s.executeQuery("select name from Users;");
+            while (rs.next()) {
+                System.out.println(rs.getString("name"));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
