dab6d00995003cbbf66e38182890d6adb6bafb8a
(Student  C00291296) Ihor Antonov <C00291296@setu.ie>
2024-02-08 09:11:09 +0000

    Add methods to Moderator class according to the spec

--- Moderator.java
+++ Moderator.java
@@ -3,4 +3,12 @@ public class Moderator extends User {
     public Moderator(int id, ForumDB fdb) {
         super(id, fdb);
     }
+
+    public void censorPost(Post post) {
+        post.getCensored();
+    }
+
+    public void createSubforum(String name) {
+        getForumDB().addSubforum(name);
+    }
 }
