6e4f869e6e3014280bf88aa71737bc26084a89bf
(Student  C00291296) Ihor Antonov <C00291296@setu.ie>
2024-03-12 09:29:33 +0000

    Add a test JFrame

--- Program.java
+++ Program.java
@@ -2,9 +2,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import javax.swing.JFrame;
+import javax.swing.LayoutStyle;
 
 public class Program {
+    
     public static void main(String[] args) {
+        var frame = new JFrame("The Forum", null);
+        frame.setVisible(true);
+        frame.setSize(800, 600);
+        frame.setResizable(false);
+
         ForumDB fdb = new ForumDB();
         fdb.printUsers();
         fdb.addSubforum("Ostriches");
