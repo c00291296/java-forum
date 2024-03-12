8ccf05fef067967f395946cfe7aa1fb953e4ccc1
(Student  C00291296) Ihor Antonov <C00291296@setu.ie>
2024-03-12 09:56:30 +0000

    Add test dummy for subforum list on the left in GUI

--- Program.java
+++ Program.java
@@ -2,16 +2,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
 import javax.swing.JFrame;
-import javax.swing.LayoutStyle;
+import java.awt.BorderLayout;
+
+import javax.swing.JList;
 
 public class Program {
     
     public static void main(String[] args) {
         var frame = new JFrame("The Forum", null);
+        var list = new JList<String>(new String[] {"aaa aaa aaa", "b bbb of b bb", "ccc ccc and ccc of ccc"});
+        var layout = new BorderLayout(0, 0);
+        list.setVisible(true);
         frame.setVisible(true);
         frame.setSize(800, 600);
         frame.setResizable(false);
+        frame.setLayout(layout);
+        frame.add(list, layout.WEST);
+
 
         ForumDB fdb = new ForumDB();
         fdb.printUsers();
