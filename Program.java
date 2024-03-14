d76769ecd5407b596f0de7b0e35ce66bc05a1166
(Student  C00291296) Ihor Antonov <C00291296@setu.ie>
2024-03-14 09:08:31 +0000

    Make the list on the left actually display the subforums

--- Program.java
+++ Program.java
@@ -9,21 +9,27 @@ import java.awt.BorderLayout;
 import javax.swing.JList;
 
 public class Program {
-    
+    final static int WINDOW_WIDTH = 800;
+    final static int WINDOW_HEIGHT = 600;
     public static void main(String[] args) {
         var frame = new JFrame("The Forum", null);
-        var list = new JList<String>(new String[] {"aaa aaa aaa", "b bbb of b bb", "ccc ccc and ccc of ccc"});
-        var layout = new BorderLayout(0, 0);
-        list.setVisible(true);
-        frame.setVisible(true);
-        frame.setSize(800, 600);
-        frame.setResizable(false);
-        frame.setLayout(layout);
-        frame.add(list, layout.WEST);
 
 
         ForumDB fdb = new ForumDB();
-        fdb.printUsers();
-        fdb.addSubforum("Ostriches");
+        Forum f = new Forum(fdb);
+        Subforum[] subforums = f.getSubforums();
+        String[] subforumNames = new String[subforums.length];
+        for (int i = 0; i < subforums.length; i++) {
+            subforumNames[i] = subforums[i].getTitle();
+        }
+        var list = new JList<String>(subforumNames);
+        list.setFixedCellWidth(WINDOW_WIDTH / 5);
+        var layout = new BorderLayout(0, 0);
+        list.setVisible(true);
+        frame.setVisible(true);
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.setResizable(false);
+        frame.setLayout(layout);
+        frame.add(list, layout.WEST);
     }
 }
