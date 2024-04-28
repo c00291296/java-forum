534d25154bdeeaafb4ae90e091206cd3cc364af9
Ihor Antonov <C00291296@setu.ie>
2024-04-28 21:40:37 +0100

    Add logging!

--- Program.java
+++ Program.java
@@ -1,16 +1,26 @@
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.io.FileOutputStream;
+
 import javax.swing.JTextArea;
 
 
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import java.io.PrintStream;
+import java.io.FileOutputStream;
 
 public class Program {
     public static void main(String[] args) {
-
+        try {
+        PrintStream ps = new PrintStream(new FileOutputStream("log.txt"));
+        System.setOut(ps);
+        System.setErr(ps);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
 
         ForumDB fdb = new ForumDB();
         Forum f = new Forum(fdb);
